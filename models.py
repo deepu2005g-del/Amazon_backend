6743523e423f8762b3937ca1bf91fe1278e6ff35
deepu2005g-del <deepu2005g@gmail.com>
2026-02-13 15:19:23 +0530

    push

--- models.py
+++ models.py
@@ -1,1 +1,12 @@
+from sqlalchemy import Column, Integer, String, Boolean
 from db import Base
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    email = Column(String)
+    password = Column(String)
+    address = Column(String) 
+    phone = Column(String)
+    isprime= Column(Boolean)
